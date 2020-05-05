270208bffc9a48076d37327d4707e98a11acc298
zaheersani <sani.zaheer@gmail.com>
2020-05-05 17:44:05 +0500

    HomeScreen without navigation

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>(Screen 1)</Text>
+      <Text>Welcome to My App</Text>
+      <Button title="Start!"></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
